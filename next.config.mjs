87c0d7ca1b1134433b069ca492c7dd2a666b73d9
Aziz Azizli <azizliaziz025@gmail.com>
2024-06-26 20:31:13 +0400

    try to fix problem

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,11 @@ const nextConfig = {
     transpilePackages: ['@mui/x-charts'],
     images: {
       domains: ['firebasestorage.googleapis.com','res.cloudinary.com'],
-    },
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME
+    }
 };
 
 export default nextConfig;
